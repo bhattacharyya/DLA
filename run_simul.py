f541135a6e1425699107ad165fcc1a85ef4b85d1
Shantanu Bhattacharyya <ssbhatta@scripps.edu>
2016-06-04 02:31:05 -0700

    Script to run the DLA simulation

--- run_simul.py
+++ run_simul.py
@@ -1,27 +1,23 @@
 #!/usr/bin/python
 
 import dla_simul
+
+totalParticles = int(raw_input("Total particles to simulate"))
 count = 0
-x = 250
-y = 250
-coords = []
 
 test = dla_simul.dlagrid()
 test.createGrid()
 
-for k in range(1,501):
+for k in range(1,totalParticles+1):
 	count += 1
 	test.newParticle()
 	while test.isStuck() == False :
-		#count += 1
 		test.diffuse()
-	(a,b) = test.getPosition()
-	coords.append(a)
-	coords.append(b)
-	coordfile = open("coord.txt", "w")
-	for i in list(coords):
-		coordfile = open("coord.txt", "a")
-		coordfile.write(str(i) + " ")
-		coordfile.close()
 
+	print "Particle number : " + str(count) 
+	if count % 1000 == 0:
+		test.getGrid("grid.txt")
 
+test.showGrid()
+
+test.getGrid("grid.txt")

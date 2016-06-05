b2368d374dc9141f412b87f52bc20a5fcb4f3253
Shantanu Bhattacharyya <ssbhatta@scripps.edu>
2016-06-04 22:15:02 -0700

    Put in the equation

--- analyze.py
+++ analyze.py
@@ -2,7 +2,10 @@
 import util
 
 filename = raw_input("Enter the matrix filename to be analyzed : ")
-util.createCoords(filename, "coord.txt")
-print avgNeighbor("grid.txt")
+#util.createCoords(filename, "coord.txt")
+anp = util.avgNeighbor("grid.txt")
 
+a = (-0.9492 * anp) + 2.3636
+b = 10**a
 
+print "\n Best guess for kappa is : ", b

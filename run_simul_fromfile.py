8442b9bdd9088407f37a297f99a411e36b0efcd6
Shantanu Bhattacharyya <ssbhatta@scripps.edu>
2016-06-06 17:27:16 -0700

    Starts the simulation from a grid.txt file

---
+++ run_simul_fromfile.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+
+import dla_simul
+
+count = 0
+
+test = dla_simul.dlagrid()
+test.readGridFile("grid.txt")
+
+tenpercent = test.numberOfParticles()/10
+for k in range(1,test.numberOfParticles()+1):
+	count += 1
+	test.newParticle()
+	while test.isStuck() == False :
+		test.diffuse()
+
+	if count % tenpercent == 0:
+		print str(count) + " particles done"
+		test.getGrid("grid.txt")
+
+test.showGrid()
+
+print "\nSimulation Over ; matrix output as grid.txt \n"
+
+test.getGrid("grid.txt")
